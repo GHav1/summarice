79f5155a3c370872fdde2c125caf64501a95003c
GHav1 <adamonevalerio1@gmail.com>
2025-10-30 05:59:28 +0800

    Update admin_dashboard.php

--- summarice/admin_dashboard.php
+++ summarice/admin_dashboard.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['username']) || $_SESSION['username'] !== 'admin') {
-    header("Location: loginpage.html");
+    header("Location: loginpage.php");
     exit;
 }
 ?>
